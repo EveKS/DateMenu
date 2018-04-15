5279457bd822c069197f0c5f92450801d238c278
EveKS <vikkes@yandex.ru>
2018-04-15 23:42:33 +0700

    initial
    initial

---
+++ js/site.js
@@ -0,0 +1,26 @@
+﻿$('.date-end').datetimepicker({
+    locale: 'ru',
+    format: 'DD MMM YYYY',
+    defaultDate: moment($('.date-end').data('value'), 'DD-MM-YYYY').format('DD MMM YYYY'),
+    maxDate: moment($('.date-end').data('rdate'), 'DD-MM-YYYY').format('DD MMM YYYY'),
+    minDate: $('.date-begin').data('value')
+        ? moment($('.date-begin').data('value'), 'DD-MM-YYYY').format('DD MMM YYYY')
+        : moment($('.date-end').data('ldate'), 'DD-MM-YYYY').format('DD MMM YYYY'),
+    useCurrent: false
+}).on('dp.change', function (e) {
+    $('.date-begin').data('DateTimePicker').minDate(e.date);
+});
+
+$('.date-begin').datetimepicker({
+    locale: 'ru',
+    format: 'DD MMM YYYY',
+    defaultDate: moment($('.date-begin').data('value'), 'DD-MM-YYYY').format('DD MMM YYYY'),
+    maxDate: $('.date-end').data('value')
+        ? moment($('.date-end').data('value'), 'DD-MM-YYYY').format('DD MMM YYYY')
+        : moment($('.date-begin').data('rdate'), 'DD-MM-YYYY').format('DD MMM YYYY'),
+    minDate: moment($('.date-begin').data('ldate'), 'DD-MM-YYYY').format('DD MMM YYYY'),
+    useCurrent: false
+})
+.on('dp.change', function (e) {
+    $('.date-end').data('DateTimePicker').maxDate(e.date);
+});
